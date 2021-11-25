cd84ace102c6a3e193f93ac19d9d3cdef82a96cb
samsonsanusi <samsonsanusi96@gmail.com>
2021-11-25 16:53:25 +0100

    renamed classes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import Search from "./search";
-import Homepage from "./homePage";
-import Representative_details from "./representative_details"
-import Representatives from "./representatives";
+import Search from "./screen/search";
+import Homepage from "./screen/homePage";
+import Representative_details from "./screen/representative_details"
+import Representatives from "./screen/representatives";
 import { Route, Routes } from "react-router-dom";
 
 
 function App(){
   return (
     <Routes>
-      <Route path="/" element={<Representative_details />} />
+      <Route path="/" element={<Homepage/>} />
       <Route path="/search" element={<Search/>} />
       <Route path="/reps" element={<Representatives />} />
+      <Route path="/reps_details" element={<Representative_details />} />
     </Routes>
   )
 }
---
+++ src/components/headers/headerTwo.js
@@ -0,0 +1,38 @@
+import Logo from "../../assets/icons/Logo.png";
+import {useState} from "react";
+import Hamburger from 'hamburger-react';
+
+
+const HeaderTwo = () =>{
+    const [isOpen, setOpen] = useState(false)
+    const contentStyle = {
+        height: '160px',
+        color: '#fff',
+        lineHeight: '160px',
+        textAlign: 'center',
+        background: '#364d79',
+      };
+    return (
+      <>
+            <div className="clipPath">
+                <svg width="135" height="40" viewBox="0 0 135 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="70.5" cy="-30.5" r="45.5" stroke="url(#paint0_linear_25:117)" stroke-width="50"/>
+                <defs>
+                <linearGradient id="paint0_linear_25:117" x1="25" y1="-76" x2="25" y2="15" gradientUnits="userSpaceOnUse">
+                <stop offset="0.538462" stop-color="#90DF6F"/>
+                <stop offset="1" stop-color="#00973F"/>
+                </linearGradient>
+                </defs>
+                </svg>
+            </div>  
+            <div className="header">
+                <div className="logo"><img src={Logo}/></div>
+                <div className="hamburger">
+                <Hamburger toggled={isOpen} toggle={setOpen} />
+                </div>
+            </div>
+            </>
+    )
+}
+
+export default HeaderTwo;
