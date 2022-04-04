f75fbafc39466f01d7f76bab8e70e76c6100266e
samsonsanusi <samsonsanusi96@gmail.com>
2022-04-04 12:25:57 +0100

    fix/samson/changes

--- src/components/headers/headerOne.js
+++ src/components/headers/headerOne.js
@@ -1,30 +1,35 @@
 import Logo_light from "../../assets/icons/Logo_light.svg";
-import {useState} from "react";
-import Hamburger from 'hamburger-react';
+import { useState } from "react";
+import Hamburger from "hamburger-react";
 import Oval_colored from "../../assets/icons/Oval_colored.svg";
 
-    
 const HeaderOne = () => {
-    const [isOpen, setOpen] = useState(false)
-    return(
+    const [isOpen, setOpen] = useState(false);
+    return (
         <div>
             <header className="representative_header">
-                <div className="header_dark" >
-                <div className="header_logo"><img className="logo_white" src={Logo_light} alt=""/></div>
-                <div className="hamburger_light">
-                <Hamburger toggled={isOpen} toggle={setOpen} />
-                </div>
+                <div className="header_dark">
+                    <div className="header_logo">
+                        <img className="logo_white" src={Logo_light} alt="" />
+                    </div>
+                    <div className="hamburger_light">
+                        <Hamburger toggled={isOpen} toggle={setOpen} />
+                    </div>
                 </div>
                 <div className="search_representative">
                     <form className="representative_form">
-                    <input type="text" className="representative_area_search" placeholder="Anthony Ajayi, Onike, Yaba, Lagos" />
+                        <input
+                            type="text"
+                            className="representative_area_search"
+                            placeholder="Anthony Ajayi, Onike, Yaba, Lagos"
+                        />
                     </form>
                     <div className="searchBtn_representative">
-                    <img src={Oval_colored} alt=""/>
+                        <img src={Oval_colored} alt="" />
                     </div>
                 </div>
             </header>
         </div>
-    )
-}
+    );
+};
 export default HeaderOne;
